570c3a2475e7d100949fbe638cec6c8916a82893
jonathan-robin <jonathan@levelup-analytics.com>
2025-02-08 16:13:59 +0400

    tradeService javadoc

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/TradeService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/TradeService.java
@@ -18,76 +18,101 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class TradeService {
-		
-	@Autowired
-	private TradeRepository tradeRepository;
-	
-	public List<Trade> findAllTrades(){ 
-		return tradeRepository.findAll();
-	}
-	
-	public void saveTrade(Trade trade){ 	
-		Trade newTrade = new Trade(); 
-		newTrade.setAccount(trade.getAccount());
-		newTrade.setBuyQuantity(trade.getBuyQuantity());
-		newTrade.setType(trade.getType());
+    
+    @Autowired
+    private TradeRepository tradeRepository;
+    
+    /**
+     * Retrieves all trades from the repository.
+     * 
+     * @return a list of all trades
+     */
+    public List<Trade> findAllTrades(){ 
+        return tradeRepository.findAll();
+    }
+    
+    /**
+     * Saves a new trade in the repository.
+     * 
+     * @param trade the trade to save
+     */
+    public void saveTrade(Trade trade){    
+        Trade newTrade = new Trade(); 
+        newTrade.setAccount(trade.getAccount());
+        newTrade.setBuyQuantity(trade.getBuyQuantity());
+        newTrade.setType(trade.getType());
 
-    	log.info("Saving new trade {}...", trade);
-    	tradeRepository.save(trade);
-	}
-	
-	public Trade findById(Integer id) throws Exception { 
-		Optional<Trade> trade = tradeRepository.findById(id); 
-		
-		if (trade.isPresent())
-			return trade.get(); 
-		
-		else 
-			throw new Exception("can't retrieve trade with id " + id);
-	}
-	
-	
-	public Trade updateTrade(Trade trade) throws Exception { 
-		
-		Optional<Trade> opt = tradeRepository.findById(trade.getTradeId());
-		Map<String, String> tmpUpdates = new HashMap<>();
-		if (opt.isPresent()) { 
-			Trade old = opt.get();
+        log.info("Saving new trade {}...", trade);
+        tradeRepository.save(trade);
+    }
+    
+    /**
+     * Finds a trade by its ID.
+     * 
+     * @param id the ID of the trade to find
+     * @return the trade with the specified ID
+     * @throws Exception if the trade is not found
+     */
+    public Trade findById(Integer id) throws Exception { 
+        Optional<Trade> trade = tradeRepository.findById(id); 
+        
+        if (trade.isPresent())
+            return trade.get(); 
+        
+        else 
+            throw new Exception("can't retrieve trade with id " + id);
+    }
+    
+    /**
+     * Updates an existing trade by comparing fields and saving changes.
+     * 
+     * @param trade the updated trade
+     * @return the updated trade
+     * @throws Exception if the trade to update is not found
+     */
+    public Trade updateTrade(Trade trade) throws Exception { 
+        
+        Optional<Trade> opt = tradeRepository.findById(trade.getTradeId());
+        Map<String, String> tmpUpdates = new HashMap<>();
+        if (opt.isPresent()) { 
+            Trade old = opt.get();
 
-		    for (Field field : Trade.class.getDeclaredFields()) {
-		        field.setAccessible(true);
-		        try {
-		            Object originalValue = field.get(old);
-		            Object updatedValue = field.get(trade);
+            for (Field field : Trade.class.getDeclaredFields()) {
+                field.setAccessible(true);
+                try {
+                    Object originalValue = field.get(old);
+                    Object updatedValue = field.get(trade);
 
-		            if (!Objects.equals(originalValue, updatedValue)) {
-		            	tmpUpdates.put(field.toString(), updatedValue.toString());
-		                field.set(old, updatedValue);
-		            }
-		        } catch (IllegalAccessException e) {
-		            e.printStackTrace();
-		        }
-		    }
-		    log.info("Updating trade id: {} with updates {}", trade.getTradeId(), tmpUpdates);
-		    tradeRepository.save(old);
-		    return findById(old.getTradeId());
-		}
-		else 
-			throw new Exception("Can't find current trade");
-		
-		
-	}
-	
-	public void deleteTradeById(Integer id) throws Exception { 
-		Optional<Trade> tradeToDelete = tradeRepository.findById(id); 
-		if (tradeToDelete.isPresent()) {
-			log.info("Deleting trade with ID: {}", id);
-			tradeRepository.deleteById(id);
-		}
-		else {
-			throw new Exception("Can't find the trade for id: " + id);
-		}
-		
-	}
-	
+                    if (!Objects.equals(originalValue, updatedValue)) {
+                        tmpUpdates.put(field.toString(), updatedValue.toString());
+                        field.set(old, updatedValue);
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            log.info("Updating trade id: {} with updates {}", trade.getTradeId(), tmpUpdates);
+            tradeRepository.save(old);
+            return findById(old.getTradeId());
+        }
+        else 
+            throw new Exception("Can't find current trade");
+    }
+    
+    /**
+     * Deletes a trade by its ID.
+     * 
+     * @param id the ID of the trade to delete
+     * @throws Exception if the trade is not found
+     */
+    public void deleteTradeById(Integer id) throws Exception { 
+        Optional<Trade> tradeToDelete = tradeRepository.findById(id); 
+        if (tradeToDelete.isPresent()) {
+            log.info("Deleting trade with ID: {}", id);
+            tradeRepository.deleteById(id);
+        }
+        else {
+            throw new Exception("Can't find the trade for id: " + id);
+        }
+    }
 }
