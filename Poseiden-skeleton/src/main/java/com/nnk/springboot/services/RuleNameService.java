94270b7bd4deed101e09ff665b573e431c7e2c33
jonathan-robin <jonathan@levelup-analytics.com>
2025-02-08 16:13:14 +0400

    javadoc ruleNameService

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/RuleNameService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/RuleNameService.java
@@ -18,81 +18,105 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class RuleNameService {
-	
-	@Autowired
-	private RuleNameRepository ruleNameRepository;
-	
-	public List<RuleName> findAllRuleNames(){ 
-		return ruleNameRepository.findAll();
-	}
-	
-	public void saveRuleName(RuleName ruleName){ 
-    	
-    	RuleName newRuleName = new RuleName(); 
-    	newRuleName.setDescription(ruleName.getDescription());
-    	newRuleName.setJson(ruleName.getJson());
-    	newRuleName.setName(ruleName.getName());
-    	newRuleName.setSqlPart(ruleName.getSqlPart());
-    	newRuleName.setSqlStr(ruleName.getSqlStr());
-    	newRuleName.setTemplate(ruleName.getTemplate());
-    	
-    	log.info("Saving new ruleName {}...", newRuleName);
-    	ruleNameRepository.save(newRuleName);
-	}
-	
-	public RuleName findById(Integer id) throws Exception { 
-		Optional<RuleName> ruleName = ruleNameRepository.findById(id); 
-		
-		if (ruleName.isPresent())
-			return ruleName.get(); 
-		
-		else 
-			throw new Exception("can't retrieve ruleName with id " + id);
-	}
-	
-	
-	public RuleName updateRuleName(RuleName ruleName) throws Exception { 
-		
-		Optional<RuleName> opt = ruleNameRepository.findById(ruleName.getId());
-		Map<String, String> tmpUpdates = new HashMap<>();
-		if (opt.isPresent()) { 
-			RuleName old = opt.get();
+    
+    @Autowired
+    private RuleNameRepository ruleNameRepository;
+    
+    /**
+     * Retrieves all rule names from the repository.
+     * 
+     * @return a list of all rule names
+     */
+    public List<RuleName> findAllRuleNames(){ 
+        return ruleNameRepository.findAll();
+    }
+    
+    /**
+     * Saves a new rule name in the repository.
+     * 
+     * @param ruleName the rule name to save
+     */
+    public void saveRuleName(RuleName ruleName){ 
+        
+        RuleName newRuleName = new RuleName(); 
+        newRuleName.setDescription(ruleName.getDescription());
+        newRuleName.setJson(ruleName.getJson());
+        newRuleName.setName(ruleName.getName());
+        newRuleName.setSqlPart(ruleName.getSqlPart());
+        newRuleName.setSqlStr(ruleName.getSqlStr());
+        newRuleName.setTemplate(ruleName.getTemplate());
+        
+        log.info("Saving new ruleName {}...", newRuleName);
+        ruleNameRepository.save(newRuleName);
+    }
+    
+    /**
+     * Finds a rule name by its ID.
+     * 
+     * @param id the ID of the rule name to find
+     * @return the rule name with the specified ID
+     * @throws Exception if the rule name is not found
+     */
+    public RuleName findById(Integer id) throws Exception { 
+        Optional<RuleName> ruleName = ruleNameRepository.findById(id); 
+        
+        if (ruleName.isPresent())
+            return ruleName.get(); 
+        
+        else 
+            throw new Exception("can't retrieve ruleName with id " + id);
+    }
+    
+    /**
+     * Updates an existing rule name by comparing fields and saving changes.
+     * 
+     * @param ruleName the updated rule name
+     * @return the updated rule name
+     * @throws Exception if the rule name to update is not found
+     */
+    public RuleName updateRuleName(RuleName ruleName) throws Exception { 
+        
+        Optional<RuleName> opt = ruleNameRepository.findById(ruleName.getId());
+        Map<String, String> tmpUpdates = new HashMap<>();
+        if (opt.isPresent()) { 
+            RuleName old = opt.get();
 
-		    for (Field field : RuleName.class.getDeclaredFields()) {
-		        field.setAccessible(true);
-		        try {
-		            Object originalValue = field.get(old);
-		            Object updatedValue = field.get(ruleName);
+            for (Field field : RuleName.class.getDeclaredFields()) {
+                field.setAccessible(true);
+                try {
+                    Object originalValue = field.get(old);
+                    Object updatedValue = field.get(ruleName);
 
-		            if (!Objects.equals(originalValue, updatedValue)) {
-		            	tmpUpdates.put(field.toString(), updatedValue.toString());
-		                field.set(old, updatedValue);
-		            }
-		        } catch (IllegalAccessException e) {
-		            e.printStackTrace();
-		        }
-		    }
-		    log.info("Updating ruleName id: {} with updates {}", ruleName.getId(), tmpUpdates);
-		    ruleNameRepository.save(old);
-		    return findById(old.getId());
-		}
-		else 
-			throw new Exception("Can't find current Bid");
-		
-		
-	}
-	
-	public void deleteRuleNameById(Integer id) throws Exception { 
-		Optional<RuleName> ruleNameToDelete = ruleNameRepository.findById(id); 
-		if (ruleNameToDelete.isPresent()) {
-			log.info("Deleting ruleName with ID: {}", id);
-			ruleNameRepository.deleteById(id);
-		}
-		else {
-			throw new Exception("Can't find the ruleName for id: " + id);
-		}
-		
-	}
+                    if (!Objects.equals(originalValue, updatedValue)) {
+                        tmpUpdates.put(field.toString(), updatedValue.toString());
+                        field.set(old, updatedValue);
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            log.info("Updating ruleName id: {} with updates {}", ruleName.getId(), tmpUpdates);
+            ruleNameRepository.save(old);
+            return findById(old.getId());
+        }
+        else 
+            throw new Exception("Can't find current Bid");
+    }
+    
+    /**
+     * Deletes a rule name by its ID.
+     * 
+     * @param id the ID of the rule name to delete
+     * @throws Exception if the rule name is not found
+     */
+    public void deleteRuleNameById(Integer id) throws Exception { 
+        Optional<RuleName> ruleNameToDelete = ruleNameRepository.findById(id); 
+        if (ruleNameToDelete.isPresent()) {
+            log.info("Deleting ruleName with ID: {}", id);
+            ruleNameRepository.deleteById(id);
+        }
+        else {
+            throw new Exception("Can't find the ruleName for id: " + id);
+        }
+    }
 }
-
-
