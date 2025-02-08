dcb1db2e4bdd1b404d5d9392ad404aafab359524
jonathan-robin <jonathan@levelup-analytics.com>
2025-02-08 13:09:45 +0400

    add ruleName controller +service

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -1,6 +1,15 @@
 package com.nnk.springboot.controllers;
 
+import com.nnk.springboot.domain.Rating;
 import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.services.RatingService;
+import com.nnk.springboot.services.RuleNameService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,46 +18,98 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class RuleNameController {
-    // TODO: Inject RuleName service
 
-    @RequestMapping("/ruleName/list")
-    public String home(Model model)
-    {
-        // TODO: find all RuleName, add to model
-        return "ruleName/list";
-    }
+ 	@Autowired
+ 	private RuleNameService ruleNameService;
+ 	
+ 	 public String getUser(@AuthenticationPrincipal UserDetails userDetails) {
+ 	        return "User Details: " + userDetails.getUsername();
+     }
 
-    @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleName bid) {
-        return "ruleName/add";
-    }
+     @RequestMapping("/ruleName/list")
+     public String home(Model model, @AuthenticationPrincipal UserDetails userDetails, HttpServletRequest request) {
+     	String remoteUser = request.getRemoteUser();
+         model.addAttribute("ratings", ruleNameService.findAllRuleNames());
+         model.addAttribute("remoteUser", remoteUser);
+         return "ruleName/list";
+     }
+     
+     @GetMapping("/ruleName/add")
+     public String addRatingForm(RuleName ruleName, Model model) {
+     	model.addAttribute("ruleName", new RuleName());
+         return "ruleName/add";
+     }
 
-    @PostMapping("/ruleName/validate")
-    public String validate(@Valid RuleName ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
-        return "ruleName/add";
-    }
+     @PostMapping("/ruleName/validate")
+     public String validate(@Valid RuleName ruleName, BindingResult result, Model model) {
+         // TODO: check data valid and save to db, after saving return Curve list
+     	log.info("call to POST /ruleName/validate with {}", ruleName.toString());
 
-    @GetMapping("/ruleName/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
-        return "ruleName/update";
-    }
-
-    @PostMapping("/ruleName/update/{id}")
-    public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleName ruleName,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
-        return "redirect:/ruleName/list";
-    }
-
-    @GetMapping("/ruleName/delete/{id}")
-    public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
-        return "redirect:/ruleName/list";
-    }
+   	  // Si des erreurs de validation sont présentes, renvoyer la vue avec les erreurs
+       if (result.hasErrors()) {
+     	  log.info("errors: {}", result.getAllErrors());
+     	  return "ruleName/add"; // Ou toute autre vue qui montre les erreurs de validation
+       }
+       
+       ruleNameService.saveRuleName(ruleName);
+       List<RuleName> ruleNames = ruleNameService.findAllRuleNames();
+       
+       model.addAttribute("ruleNames", ruleNames);
+       return "ruleName/list";  // Rediriger vers la liste si tout va bien
+     }
+     
+     @GetMapping("/ruleName/update/{id}")
+     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
+         // TODO: get CurvePoint by Id and to model then show to the form
+     	log.info("call to GET /ruleName/update/{}", id);
+     	try { 
+     		RuleName ruleName = ruleNameService.findById(id);  
+     		model.addAttribute("ruleName", ruleName);
+     		return "ruleName/update";
+     	}
+     	catch (Exception ex) { 
+     		log.warn("Exception : {}", ex);
+     		return null;
+     	}
+     }
+     
+     @PostMapping("/ruleName/update/{id}")
+     public String updateRating(@PathVariable("id") Integer id, @jakarta.validation.Valid RuleName ruleName,
+                              BindingResult result, Model model) throws Exception{
+         // TODO: check required fields, if valid call service to update Curve and return Curve list
+     	if (result.hasErrors()) 
+     		return null; 
+     	
+     	if (ruleName.getDescription() != null && ruleName.getJson() != null  && ruleName.getName() != null && ruleName.getSqlPart() != null
+     			&& ruleName.getSqlStr() != null && ruleName.getTemplate() != null) { 
+     		ruleNameService.updateRuleName(ruleName);
+     		List<RuleName> ruleNames = ruleNameService.findAllRuleNames();
+     		model.addAttribute("ruleNames", ruleNames);
+             return "redirect:/ruleName/list";
+     	} else { 
+     		throw new Exception("Error in form, can't update ruleName.");
+     	}
+     }
+     
+     @GetMapping("/ruleName/delete/{id}")
+     public String deleteRating(@PathVariable("id") Integer id, Model model) {
+     	log.info("Calling GET /ruleName/delete/{}", id);
+     	try {	
+ 	    	ruleNameService.deleteRuleNameById(id);
+     	}
+     	catch (Exception e) { 
+     		log.warn(e.getMessage());
+     	}
+     	List<RuleName> ruleNames = ruleNameService.findAllRuleNames();
+     	model.addAttribute("ruleNames", ruleNames);
+         return "redirect:/ruleName/list";
+     }
+     
 }
---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/RuleNameService.java
@@ -0,0 +1,101 @@
+package com.nnk.springboot.services;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.repositories.RatingRepository;
+import com.nnk.springboot.repositories.RuleNameRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class RuleNameService {
+	
+	@Autowired
+	private RuleNameRepository ruleNameRepository;
+	
+	public List<RuleName> findAllRuleNames(){ 
+		return ruleNameRepository.findAll();
+	}
+	
+	public void saveRuleName(RuleName ruleName){ 
+    	
+    	RuleName newRuleName = new RuleName(); 
+    	newRuleName.setDescription(ruleName.getDescription());
+    	newRuleName.setJson(ruleName.getJson());
+    	newRuleName.setName(ruleName.getName());
+    	newRuleName.setSqlPart(ruleName.getSqlPart());
+    	newRuleName.setSqlStr(ruleName.getSqlStr());
+    	newRuleName.setTemplate(ruleName.getTemplate());
+    	
+    	log.info("Saving new ruleName {}...", newRuleName);
+    	ruleNameRepository.save(newRuleName);
+	}
+	
+	public RuleName findById(Integer id) throws Exception { 
+		Optional<RuleName> ruleName = ruleNameRepository.findById(id); 
+		
+		if (ruleName.isPresent())
+			return ruleName.get(); 
+		
+		else 
+			throw new Exception("can't retrieve ruleName with id " + id);
+	}
+	
+	
+	public RuleName updateRuleName(RuleName ruleName) throws Exception { 
+		
+		Optional<RuleName> opt = ruleNameRepository.findById(ruleName.getId());
+		Map<String, String> tmpUpdates = new HashMap<>();
+		if (opt.isPresent()) { 
+			RuleName old = opt.get();
+
+		    for (Field field : Rating.class.getDeclaredFields()) {
+		        field.setAccessible(true);
+		        try {
+		            Object originalValue = field.get(old);
+		            Object updatedValue = field.get(ruleName);
+
+		            if (!Objects.equals(originalValue, updatedValue)) {
+		            	tmpUpdates.put(field.toString(), updatedValue.toString());
+		                field.set(old, updatedValue);
+		            }
+		        } catch (IllegalAccessException e) {
+		            e.printStackTrace();
+		        }
+		    }
+		    log.info("Updating ruleName id: {} with updates {}", ruleName.getId(), tmpUpdates);
+		    ruleNameRepository.save(old);
+		    return findById(old.getId());
+		}
+		else 
+			throw new Exception("Can't find current Bid");
+		
+		
+	}
+	
+	public void deleteRuleNameById(Integer id) throws Exception { 
+		Optional<RuleName> ruleNameToDelete = ruleNameRepository.findById(id); 
+		if (ruleNameToDelete.isPresent()) {
+			log.info("Deleting ruleName with ID: {}", id);
+			ruleNameRepository.deleteById(id);
+		}
+		else {
+			throw new Exception("Can't find the ruleNale for id: " + id);
+		}
+		
+	}
+	
+}
+
+
