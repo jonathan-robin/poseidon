ed6a3ce6168b4462be6fab1bf6f1e75dc5813ec3
jonathan-robin <jonathan@levelup-analytics.com>
2025-02-08 16:28:31 +0400

    tradeController ajvadoc

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/TradeController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/TradeController.java
@@ -1,8 +1,6 @@
 package com.nnk.springboot.controllers;
 
-import com.nnk.springboot.domain.RuleName;
 import com.nnk.springboot.domain.Trade;
-import com.nnk.springboot.services.RuleNameService;
 import com.nnk.springboot.services.TradeService;
 
 import java.util.List;
@@ -10,110 +8,157 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
+/**
+ * Controller for managing Trade related operations.
+ */
+@RestController
 @Slf4j
 public class TradeController {
-	
-	@Autowired
- 	private TradeService tradeService;
- 	
- 	 public String getUser(@AuthenticationPrincipal UserDetails userDetails) {
- 	        return "User Details: " + userDetails.getUsername();
-     }
- 	 
-	@RequestMapping("/trade/list")
-	public String home(Model model, @AuthenticationPrincipal UserDetails userDetails, HttpServletRequest request) {
-	  String remoteUser = request.getRemoteUser();
-      model.addAttribute("trades", tradeService.findAllTrades());
-      model.addAttribute("remoteUser", remoteUser);
-      return "trade/list";
-	}
-	
-	@GetMapping("/trade/add")
+
+    @Autowired
+    private TradeService tradeService;
+
+    /**
+     * Retrieves the details of the authenticated user.
+     * 
+     * @param userDetails the authenticated user details
+     * @return a string containing user details
+     */
+    public String getUser(@AuthenticationPrincipal UserDetails userDetails) {
+        return "User Details: " + userDetails.getUsername();
+    }
+
+    /**
+     * Displays the list of all trades.
+     * 
+     * @param model the model to add attributes
+     * @param userDetails the authenticated user details
+     * @param request the HTTP request
+     * @return the view name for displaying the trade list
+     */
+    @RequestMapping("/trade/list")
+    public String home(Model model, @AuthenticationPrincipal UserDetails userDetails, HttpServletRequest request) {
+        String remoteUser = request.getRemoteUser();
+        model.addAttribute("trades", tradeService.findAllTrades());
+        model.addAttribute("remoteUser", remoteUser);
+        return "trade/list";
+    }
+
+    /**
+     * Displays the form to add a new trade.
+     * 
+     * @param trade the trade object
+     * @param model the model to add attributes
+     * @return the view name for the trade add form
+     */
+    @GetMapping("/trade/add")
     public String addRatingForm(Trade trade, Model model) {
-    	model.addAttribute("trade", new Trade());
+        model.addAttribute("trade", new Trade());
         return "trade/add";
     }
-	
+
+    /**
+     * Validates and saves a new trade.
+     * 
+     * @param trade the trade to be saved
+     * @param result the result of binding the trade object
+     * @param model the model to add attributes
+     * @return the view name for displaying the updated trade list
+     */
     @PostMapping("/trade/validate")
     public String validate(@Valid Trade trade, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Curve list
-   	 log.info("call to POST /ruleName/validate with {}", trade.toString());
+        log.info("call to POST /trade/validate with {}", trade.toString());
 
-  	  // Si des erreurs de validation sont présentes, renvoyer la vue avec les erreurs
-      if (result.hasErrors() ) {
-    	  log.info("errors: {}", result.getAllErrors());
-    	  return "trade/add"; // Ou toute autre vue qui montre les erreurs de validation
-      }
-      else if (trade.getAccount() == null || trade.getAccount().isEmpty()) {
-     	  log.info("Account can't be null");
-     	  return "trade/add"; // Ou toute autre vue qui montre les erreurs de validation
-      }
-      
-      tradeService.saveTrade(trade);
-      List<Trade> trades = tradeService.findAllTrades();
-      
-      model.addAttribute("trades", trades);
-      return "trade/list";  // Rediriger vers la liste si tout va bien
+        if (result.hasErrors()) {
+            log.info("errors: {}", result.getAllErrors());
+            return "trade/add";
+        } else if (trade.getAccount() == null || trade.getAccount().isEmpty()) {
+            log.info("Account can't be null");
+            return "trade/add";
+        }
+
+        tradeService.saveTrade(trade);
+        List<Trade> trades = tradeService.findAllTrades();
+
+        model.addAttribute("trades", trades);
+        return "trade/list";
     }
-      
+
+    /**
+     * Displays the form to update an existing trade.
+     * 
+     * @param id the ID of the trade to update
+     * @param model the model to add attributes
+     * @return the view name for the trade update form
+     */
     @GetMapping("/trade/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get CurvePoint by Id and to model then show to the form
-    	log.info("call to GET /trade/update/{}", id);
-    	try { 
-    		Trade trade = tradeService.findById(id);  
-    		model.addAttribute("trade", trade);
-    		return "trade/update";
-    	}
-    	catch (Exception ex) { 
-    		log.warn("Exception : {}", ex);
-    		return null;
-    	}
+        log.info("call to GET /trade/update/{}", id);
+        try {
+            Trade trade = tradeService.findById(id);
+            model.addAttribute("trade", trade);
+            return "trade/update";
+        } catch (Exception ex) {
+            log.warn("Exception : {}", ex);
+            return null;
+        }
     }
 
-
+    /**
+     * Validates and updates an existing trade.
+     * 
+     * @param id the ID of the trade to update
+     * @param trade the trade object with updated data
+     * @param result the result of binding the trade object
+     * @param model the model to add attributes
+     * @return the view name for redirecting to the trade list after update
+     * @throws Exception if there is an error in the form submission
+     */
     @PostMapping("/trade/update/{id}")
     public String updateRating(@PathVariable("id") Integer id, @jakarta.validation.Valid Trade trade,
-                             BindingResult result, Model model) throws Exception{
-        // TODO: check required fields, if valid call service to update Curve and return Curve list
-    	if (result.hasErrors()) 
-    		return null; 
-    	
-    	if (trade.getAccount() != null && trade.getType() != null  && trade.getBuyQuantity() != null) { 
-    		tradeService.updateTrade(trade);
-    		List<Trade> trades = tradeService.findAllTrades();
-    		model.addAttribute("trades", trades);
+                               BindingResult result, Model model) throws Exception {
+        if (result.hasErrors())
+            return null;
+
+        if (trade.getAccount() != null && trade.getType() != null && trade.getBuyQuantity() != null) {
+            tradeService.updateTrade(trade);
+            List<Trade> trades = tradeService.findAllTrades();
+            model.addAttribute("trades", trades);
             return "redirect:/trade/list";
-    	} else { 
-    		throw new Exception("Error in form, can't update trade.");
-    	}
+        } else {
+            throw new Exception("Error in form, can't update trade.");
+        }
     }
 
+    /**
+     * Deletes a trade by its ID.
+     * 
+     * @param id the ID of the trade to delete
+     * @param model the model to add attributes
+     * @return the view name for redirecting to the trade list after deletion
+     */
     @GetMapping("/trade/delete/{id}")
     public String deleteRating(@PathVariable("id") Integer id, Model model) {
-    	log.info("Calling GET /trade/delete/{}", id);
-    	try {	
-	    	tradeService.deleteTradeById(id);
-    	}
-    	catch (Exception e) { 
-    		log.warn(e.getMessage());
-    	}
-    	List<Trade> trades = tradeService.findAllTrades();
-    	model.addAttribute("trades", trades);
+        log.info("Calling GET /trade/delete/{}", id);
+        try {
+            tradeService.deleteTradeById(id);
+        } catch (Exception e) {
+            log.warn(e.getMessage());
+        }
+        List<Trade> trades = tradeService.findAllTrades();
+        model.addAttribute("trades", trades);
         return "redirect:/trade/list";
     }
-    
 }
