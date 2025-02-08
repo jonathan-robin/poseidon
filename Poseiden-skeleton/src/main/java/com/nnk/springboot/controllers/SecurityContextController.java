a2fe1df74a2f6b4cae2292b5644a7cdf195d3ea0
jonathan-robin <jonathan@levelup-analytics.com>
2025-02-08 16:26:56 +0400

    restController + securitContext javadoc

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/SecurityContextController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/SecurityContextController.java
@@ -4,16 +4,30 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Controller for managing security-related actions such as retrieving the authenticated user.
+ */
 @RestController
+@Slf4j
 public class SecurityContextController {
 
+    /**
+     * Retrieves the currently logged-in user's name and roles.
+     * 
+     * @return a string representing the logged-in user and their roles, or a message indicating no user is logged in
+     */
     @GetMapping("/user")
     public String getUser() {
+        log.info("Calling GET /user to retrieve the logged-in user");
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        
         if (authentication != null && authentication.isAuthenticated()) {
+            log.info("User {} is authenticated with roles: {}", authentication.getName(), authentication.getAuthorities());
             return "Logged-in user: " + authentication.getName() + " role: " + authentication.getAuthorities();
         } else {
+            log.info("No user is logged in");
             return "No user logged in";
         }
     }
