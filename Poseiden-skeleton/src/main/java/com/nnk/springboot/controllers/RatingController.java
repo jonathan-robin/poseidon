a4ca9249a9bfcb2fbbcd96cf0e4e383e62675586
jonathan-robin <jonathan@levelup-analytics.com>
2025-02-08 16:23:52 +0400

    ratingController javadoc

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -1,8 +1,6 @@
 package com.nnk.springboot.controllers;
 
-import com.nnk.springboot.domain.CurvePoint;
 import com.nnk.springboot.domain.Rating;
-import com.nnk.springboot.services.CurveService;
 import com.nnk.springboot.services.RatingService;
 
 import java.util.List;
@@ -22,93 +20,145 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Controller for managing ratings.
+ */
 @Controller
 @Slf4j
 public class RatingController {
-    // TODO: Inject Curve Point service
-	@Autowired
-	private RatingService ratingService;
-	
-	 public String getUser(@AuthenticationPrincipal UserDetails userDetails) {
-	        return "User Details: " + userDetails.getUsername();
+
+    @Autowired
+    private RatingService ratingService;
+
+    /**
+     * Retrieves the details of the current user.
+     * 
+     * @param userDetails the current user's details
+     * @return a string with user details
+     */
+    public String getUser(@AuthenticationPrincipal UserDetails userDetails) {
+        return "User Details: " + userDetails.getUsername();
     }
 
+    /**
+     * Displays the list of all ratings.
+     * 
+     * @param model the model to be passed to the view
+     * @param request the HTTP request to get the remote user
+     * @return the view displaying the list of ratings
+     */
     @RequestMapping("/rating/list")
     public String home(Model model, @AuthenticationPrincipal UserDetails userDetails, HttpServletRequest request) {
-    	String remoteUser = request.getRemoteUser();
+        log.info("Calling GET /rating/list");
+        String remoteUser = request.getRemoteUser();
         model.addAttribute("ratings", ratingService.findAllRatings());
         model.addAttribute("remoteUser", remoteUser);
         return "rating/list";
     }
 
-
+    /**
+     * Displays the form to add a new rating.
+     * 
+     * @param rating the rating object
+     * @param model the model to be passed to the view
+     * @return the form for adding a new rating
+     */
     @GetMapping("/rating/add")
     public String addRatingForm(Rating rating, Model model) {
-    	model.addAttribute("rating", new Rating());
+        log.info("Calling GET /rating/add");
+        model.addAttribute("rating", new Rating());
         return "rating/add";
     }
 
+    /**
+     * Validates and saves the rating submitted via the form.
+     * 
+     * @param rating the rating object to be saved
+     * @param result the binding result of the form submission
+     * @param model the model to be passed to the view
+     * @return the view displaying the updated list of ratings
+     */
     @PostMapping("/rating/validate")
     public String validate(@Valid Rating rating, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Curve list
-    	log.info("call to POST /rating/validate with {}", rating.toString());
+        log.info("call to POST /rating/validate with {}", rating.toString());
 
-  	  // Si des erreurs de validation sont présentes, renvoyer la vue avec les erreurs
-      if (result.hasErrors()) {
-    	  log.info("errors: {}", result.getAllErrors());
-    	  return "rating/add"; // Ou toute autre vue qui montre les erreurs de validation
-      }
-      
-      ratingService.saveRating(rating);
-      List<Rating> ratings = ratingService.findAllRatings();
-      
-      model.addAttribute("ratings", ratings);
-      return "rating/list";  // Rediriger vers la liste si tout va bien
+        if (result.hasErrors()) {
+            log.info("errors: {}", result.getAllErrors());
+            return "rating/add";
+        }
+
+        ratingService.saveRating(rating);
+        List<Rating> ratings = ratingService.findAllRatings();
+        model.addAttribute("ratings", ratings);
+        return "rating/list"; 
     }
 
+    /**
+     * Displays the form to update an existing rating.
+     * 
+     * @param id the ID of the rating to be updated
+     * @param model the model to be passed to the view
+     * @return the form to update the rating
+     */
     @GetMapping("/rating/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get CurvePoint by Id and to model then show to the form
-    	log.info("call to GET /rating/update/{}", id);
-    	try { 
-    		Rating rating = ratingService.findById(id);  
-    		model.addAttribute("rating", rating);
-    		return "rating/update";
-    	}
-    	catch (Exception ex) { 
-    		log.warn("Exception : {}", ex);
-    		return null;
-    	}
+        log.info("call to GET /rating/update/{}", id);
+        try {
+            Rating rating = ratingService.findById(id);  
+            model.addAttribute("rating", rating);
+            return "rating/update";
+        }
+        catch (Exception ex) {
+            log.warn("Exception : {}", ex);
+            return null;
+        }
     }
 
+    /**
+     * Updates an existing rating based on the form submission.
+     * 
+     * @param id the ID of the rating to be updated
+     * @param rating the rating object with updated values
+     * @param result the binding result of the form submission
+     * @param model the model to be passed to the view
+     * @return the updated list of ratings
+     * @throws Exception if the form data is invalid
+     */
     @PostMapping("/rating/update/{id}")
-    public String updateRating(@PathVariable("id") Integer id, @jakarta.validation.Valid Rating rating,
-                             BindingResult result, Model model) throws Exception{
-        // TODO: check required fields, if valid call service to update Curve and return Curve list
-    	if (result.hasErrors()) 
-    		return null; 
-    	
-    	if (rating.getFitchRating() != null && rating.getMoodysRating() != null  && rating.getSandPRating() != null && rating.getOrderNumber() > 0) { 
-    		ratingService.updateRating(rating);
-    		List<Rating> ratings = ratingService.findAllRatings();
-    		model.addAttribute("ratings", ratings);
+    public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
+                             BindingResult result, Model model) throws Exception {
+        if (result.hasErrors()) 
+            return null;
+
+        if (rating.getFitchRating() != null && rating.getMoodysRating() != null 
+            && rating.getSandPRating() != null && rating.getOrderNumber() > 0) { 
+            ratingService.updateRating(rating);
+            List<Rating> ratings = ratingService.findAllRatings();
+            model.addAttribute("ratings", ratings);
             return "redirect:/rating/list";
-    	} else { 
-    		throw new Exception("Error in form, can't update rating.");
-    	}
+        } else { 
+            throw new Exception("Error in form, can't update rating.");
+        }
     }
 
+    /**
+     * Deletes a rating by its ID.
+     * 
+     * @param id the ID of the rating to be deleted
+     * @param model the model to be passed to the view
+     * @return the updated list of ratings
+     */
     @GetMapping("/rating/delete/{id}")
     public String deleteRating(@PathVariable("id") Integer id, Model model) {
-    	log.info("Calling GET /rating/delete/{}", id);
-    	try {	
-	    	ratingService.deleteRatingById(id);
-    	}
-    	catch (Exception e) { 
-    		log.warn(e.getMessage());
-    	}
-    	List<Rating> ratings = ratingService.findAllRatings();
-    	model.addAttribute("ratings", ratings);
+        log.info("Calling GET /rating/delete/{}", id);
+        try { 
+            ratingService.deleteRatingById(id);
+        }
+        catch (Exception e) {
+            log.warn(e.getMessage());
+        }
+        List<Rating> ratings = ratingService.findAllRatings();
+        model.addAttribute("ratings", ratings);
         return "redirect:/rating/list";
     }
 
