22016588744f7d351ad7c80928c2d156a58f3443
jonathan-robin <jonathan@levelup-analytics.com>
2025-02-08 16:20:29 +0400

    javadoc homecontroller

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
@@ -3,21 +3,36 @@ package com.nnk.springboot.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Controller for handling home page requests and redirection to admin page.
+ */
 @Controller
-public class HomeController
-{
-	@RequestMapping("/")
-	public String home(Model model)
-	{
-		return "home";
-	}
+@Slf4j
+public class HomeController {
 
-	@RequestMapping("/admin/home")
-	public String adminHome(Model model)
-	{
-		return "redirect:/bidList/list";
-	}
+    /**
+     * Displays the home page.
+     * 
+     * @param model the model to add attributes to the view
+     * @return the view name for the home page
+     */
+    @RequestMapping("/")
+    public String home(Model model) {
+        log.info("Calling GET /");
+        return "home";
+    }
 
-
+    /**
+     * Redirects to the bid list page for admin users.
+     * 
+     * @param model the model to add attributes to the view
+     * @return the redirection to the bid list page
+     */
+    @RequestMapping("/admin/home")
+    public String adminHome(Model model) {
+        log.info("Calling GET /admin/home - Redirecting to /bidList/list");
+        return "redirect:/bidList/list";
+    }
 }
