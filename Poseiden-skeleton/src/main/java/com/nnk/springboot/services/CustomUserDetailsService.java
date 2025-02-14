5e2426b0bc227d0413b221a0dd77b822d7841dc8
jonathan-robin <jonathan@levelup-analytics.com>
2025-02-14 13:54:35 +0400

    userDetails not found

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/CustomUserDetailsService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/CustomUserDetailsService.java
@@ -31,6 +31,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
 
+        if (user == null)
+        	throw new UsernameNotFoundException("Invalid credentials");
+        
         return new org.springframework.security.core.userdetails.User(user.getUsername(),
                 user.getPassword(), getGrantedAuthorities(user.getRole()));
     }
